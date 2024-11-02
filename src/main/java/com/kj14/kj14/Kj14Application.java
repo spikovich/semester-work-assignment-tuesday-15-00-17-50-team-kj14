8d11bc835c3134ccf06cb69c71094ccc69781fd6
Spich <sotrudnic003@gmail.com>
2024-11-02 01:44:38 +0100

    add spring demo

---
+++ src/main/java/com/kj14/kj14/Kj14Application.java
@@ -0,0 +1,13 @@
+package com.kj14.kj14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kj14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kj14Application.class, args);
+	}
+
+}
